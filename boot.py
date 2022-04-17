34cd62257421c78a979c047fe5bea2bfa354c16f
hchp <ph@crafteke.be>
2022-04-17 15:42:52 +0200

    example button

--- boot.py
+++ boot.py
@@ -1,18 +1,21 @@
 import sys
 from rotary_irq_rp2 import RotaryIRQ
 import time
-
+from machine import Pin
 print("Booting...")
 
 rotary_pins = [[4,6,4]] #format [DT,CLK,SW]
 rotary_controller=[None]*len(rotary_pins)
 
-button_pins=[]
+button_pins=[2,3]
+
+def button_handler(a,button_index):
+    print("b_"+str(button_index),':',a.value())
 
 for i in range(0,len(rotary_pins)):
     rotary_controller[i]=RotaryIRQ(
-        pin_num_clk=4,
-        pin_num_dt=6,
+        pin_num_dt=rotary_pins[i][0],
+        pin_num_clk=rotary_pins[i][1],
         min_val=0,
         max_val=10,
         reverse=False,
@@ -20,4 +23,6 @@ for i in range(0,len(rotary_pins)):
         pull_up=False,
         half_step=False)
 
-irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING,handler=lambda a,b=i: dtEvent(a,b))
+for i in range(0,len(button_pins)):
+    pin = Pin(button_pins[i],Pin.IN,Pin.PULL_UP)
+    pin.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING,handler=lambda a,b=i: button_handler(a,b))
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-
-
-val_old = r.value()
+#to do: array with value for each knob, check if changed, print k_X like for b_x and delta
+val_old = rotary_controller[0].value()
 while True:
-    val_new = r.value()
+    val_new = rotary_controller[0].value()
 
     if val_old != val_new:
         val_old = val_new
-        print('result =', val_new)
+        print('k_0:', val_new-val_old)
 
-    time.sleep_ms(50)
+    time.sleep_ms(10)
